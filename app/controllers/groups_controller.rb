2a5ca72842c35b66ea3faf7d95c10acc43dcc23e
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-05-24 17:48:46 +0530

    Display groups view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,11 +4,29 @@ class GroupsController < ApplicationController
   end
 
   def show
+    @user = current_user
+    @group = Group.find(params[:id])
   end
 
   def new
+    @group = Group.new
   end
 
   def create
+    @user = current_user
+    new_group = Group.new(group_params)
+    new_group.user_id = current_user.id
+    new_group.save
+    if new_group.save
+      redirect_to group_entities_path(new_group.id)
+      flash[:success] = "New category added!"
+    else
+      redirect_to new_group_path
+      flash[:alert] = "Category not added. Try again!"
+    end
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
---
+++ db/migrate/20220524090748_add_column_icon_to_groups.rb
@@ -0,0 +1,5 @@
+class AddColumnIconToGroups < ActiveRecord::Migration[7.0]
+  def change
+    add_column :groups, :icon, :string
+  end
+end
