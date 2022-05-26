cc7e7fd8c4d8b66e7d0eaf0c7141a7ba978a8d5f
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-05-26 16:55:26 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://budget-app-99.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: ENV['MAILER_URL'] }
 end
